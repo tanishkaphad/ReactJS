21c531a856123a98b721106b4f3aee7fa1e86879
tanishkaphad <taneeshka@gmail.com>
2025-09-28 15:48:00 +0530

    2 childs

--- App.js
+++ App.js
@@ -4,6 +4,10 @@
         <h1>This is the h1 tag</h1>
         <h2>This is the h2 tag</h2> -->sibling
     </div>
+     <div id="child2">
+        <h1>This is the h1 tag</h1>
+        <h2>This is the h2 tag</h2> -->sibling
+    </div>
  </div> */
 
 const parent = React.createElement(
@@ -12,8 +16,14 @@ const parent = React.createElement(
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "This is h1 tag"),
     React.createElement("h2", {}, "This is h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This is h1 tag"),
+    React.createElement("h2", {}, "This is h2 tag"),
   ])
 );
+
+//JSX
 
 console.log(parent); //object
 

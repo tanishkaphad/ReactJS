f3d6183af2f744a35272ed89a4fd4022de952f09
tanishkaphad <taneeshka@gmail.com>
2025-09-28 15:43:45 +0530

    sibling

--- App.js
+++ App.js
@@ -2,15 +2,21 @@
  <div id="parent">
     <div id="child">
         <h1>This is the h1 tag</h1>
+        <h2>This is the h2 tag</h2> -->sibling
     </div>
  </div> */
- 
- const parent = React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},
-       React.createElement("h1",{},"This is h1 tag")));
 
- console.log(parent); //object
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is h1 tag"),
+    React.createElement("h2", {}, "This is h2 tag"),
+  ])
+);
 
-        const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent); //object
 
-        root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);

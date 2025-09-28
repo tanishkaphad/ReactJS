6b3f234808aee73117b083b7b8c849278df1988b
tanishkaphad <taneeshka@gmail.com>
2025-09-28 15:39:43 +0530

    parent-child

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
- const heading = React.createElement("h1",{},"Hello World from React!");
+/*  creating structure like this:
+ <div id="parent">
+    <div id="child">
+        <h1>This is the h1 tag</h1>
+    </div>
+ </div> */
+ 
+ const parent = React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+       React.createElement("h1",{},"This is h1 tag")));
+
+ console.log(parent); //object
 
         const root = ReactDOM.createRoot(document.getElementById("root"));
 
-        root.render(heading);
+        root.render(parent);
